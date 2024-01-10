2cc0202f301f81be408a4c3bdb2fe7ba965bb450
sharon <sapot@safaricom.co.ke>
2024-01-10 22:31:08 +0300

    Update project structure

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from tuelekee.models import Base
-from tuelekee.config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_DATABASE_URI
 
 # Configure SQLite database
-from tuelekee.config import SQLALCHEMY_DATABASE_URI, engine
+from config import SQLALCHEMY_DATABASE_URI, engine
 
 # Create tables
 Base.metadata.create_all(bind=engine)
