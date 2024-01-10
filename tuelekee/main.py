eabb1d83975abb3e8e4d92c086f757cd6576d763
sharon <sapot@safaricom.co.ke>
2024-01-10 20:27:19 +0300

    Create Database Connection

--- tuelekee/main.py
+++ tuelekee/main.py
@@ -1,1 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tuelekee.models import Base
+from tuelekee.config import SQLALCHEMY_DATABASE_URI
+
+# Configure SQLite database
+from tuelekee.config import SQLALCHEMY_DATABASE_URI, engine
+
+# Create tables
+Base.metadata.create_all(bind=engine)
 
