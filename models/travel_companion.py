b56544eb29654747cfa22fa19789a1967c3b4b32
sharon <sapot@safaricom.co.ke>
2024-01-11 22:06:05 +0300

    update TravelCompanion class

--- models/travel_companion.py
+++ models/travel_companion.py
@@ -1,17 +1,15 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from . import Base
 
-class User(Base):
-    __tablename__ = "users"
+class TravelCompanion(Base):
+    __tablename__ = "travel_companions"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    name = Column(String, nullable=False)
     age = Column(Integer)
+    sex = Column(String)
 
-    # Relationship with Itinerary
-    itineraries = relationship("Itinerary", back_populates="user")
-
-    # Relationship with TravelCompanion
-    travel_companion = relationship("TravelCompanion", back_populates="user")
+    # Relationship with User
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="travel_companion")
