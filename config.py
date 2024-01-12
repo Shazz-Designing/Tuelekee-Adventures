9ba4c22ad86ec19604d9d4e51a40417e14481abc
sharon <sapot@safaricom.co.ke>
2024-01-12 12:18:32 +0300

    restarting due to the project failing to run despite debugging severally

--- config.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///tuelekee_adventures.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
